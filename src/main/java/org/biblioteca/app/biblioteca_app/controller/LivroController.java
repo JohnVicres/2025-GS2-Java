6b845db36a360d480bc1ccf6a624279e4fa7d2cb
João Vicente <166719474+JohnVicres@users.noreply.github.com>
2025-11-28 16:29:47 -0300

    controller livro

---
+++ src/main/java/org/biblioteca/app/biblioteca_app/controller/LivroController.java
@@ -0,0 +1,41 @@
+package org.biblioteca.app.biblioteca_app.controller;
+
+import org.biblioteca.app.biblioteca_app.model.Livro;
+import org.biblioteca.app.biblioteca_app.model.StatusLivro;
+import org.biblioteca.app.biblioteca_app.model.Usuario;
+import org.biblioteca.app.biblioteca_app.repository.LivroRepository;
+import org.biblioteca.app.biblioteca_app.repository.UsuarioRepository;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/livros")
+public class LivroController {
+    private final LivroRepository livroRepository;
+
+    public LivroController(LivroRepository livroRepository) {
+        this.livroRepository = livroRepository;
+    }
+
+    @GetMapping
+    public String listar(Model model) {
+        model.addAttribute("livros", livroRepository.findAll());
+        return "livros/lista";
+    }
+
+    @GetMapping("/disponiveis")
+    public String listarDisponivel(Model model) {
+        model.addAttribute("livros", livroRepository.findByStatus(StatusLivro.DISPONIVEL));
+        return "livros/listadisponiveis";
+    }
+
+    @GetMapping("/emprestados")
+    public String listarEmprestimo(Model model) {
+        model.addAttribute("livros", livroRepository.findByStatus(StatusLivro.EMPRESTADO));
+        return "livros/listaemprestados";
+    }
+}
